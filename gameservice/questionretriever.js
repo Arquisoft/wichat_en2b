5e5a0e2a773af68d38ffc7d738b86499936efaba
adrianmfuentes <amf13azul@proton.me>
2025-02-24 19:29:01 +0100

    Add question retriever route to fetch random questions

---
+++ gameservice/questionretriever.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Question = require('./question-model');
+
+const router = express.Router();
+
+// Route to get a random question
+router.get('/game', async (req, res) => {
+
+    
+});
