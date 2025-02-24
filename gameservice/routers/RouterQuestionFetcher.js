a0b42b3fb01ba1efbb648e20914631573e4260bd
Carlos Fernández Martínez <carlosfm1942@gmail.com>
2025-02-24 23:26:54 +0100

    #53 - Little explanation of fetcher

--- gameservice/routers/RouterQuestionFetcher.js
+++ gameservice/routers/RouterQuestionFetcher.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const wikiDataUri = "https://query.wikidata.org/sparql?format=json&query=";
 
 router.get('/generate/:type/:amount', async (req, res) => {
+    // Query for fetching items of a specific type from Wikidata
     var itemType = req.params['type'];
     var amount = req.params['amount'];
     const query = `
@@ -18,34 +19,36 @@ router.get('/generate/:type/:amount', async (req, res) => {
     `;
 
     try {
+        // Fetch data from Wikidata
         const url = wikiDataUri + encodeURIComponent(query);
         const response = await fetch(wikiDataUri + encodeURIComponent(query), {
             headers: {
               'User-Agent': 'wichat_en2b/1.0'
             }
         });
-          
+        
+        // Parse JSON response
         const data = await response.json();
 
-        const items = data.results.bindings.filter(item => {
-            const itemId = item.item.value.split('/').pop();
-            return item.itemLabel.value != null && item.image.value != null 
-            && !/^Q\d+$/.test(item.itemLabel.value) && itemId != item.itemLabel.value;
-        }).map(item => ({
-            name: item.itemLabel.value,
-            image: item.image.value
-        }));
+        // Filter out items with missing data
+        const items = data.results.bindings.filter(item => item.itemLabel.value != null && item.image.value != null 
+            && item.item.value.split('/')[item.item.value.split('/').size - 1] != item.itemLabel.value)
+            .map(item => ({
+                name: item.itemLabel.value,
+                image: item.image.value
+            }));
 
-        await saveQuestionstoDB(items, itemType);
+        // Save items to database and images to disk
+        await saveQuestionsToDB(items, itemType);
+
         return res.status(200).json({ message: '✅ Data fetched successfully' }); 
-
     } catch (error) {
         console.error('❌ Error fetching data:', error);
         return res.status(500).json({ error: '❌ Failed to retrieve data' });
     }
 });
 
-async function saveQuestionstoDB(items, code) {
+async function saveQuestionsToDB(items, code) {
     try {
         for (const item of items) {
             var question = await Question.findOne({ subject: code, answer: item.name });
@@ -63,6 +66,7 @@ async function saveQuestionstoDB(items, code) {
 
             await question.save();
 
+            // Save image to disk
             const imageResponse = await fetch(item.image);
             const arrayBuffer = await imageResponse.arrayBuffer();
             const imageBuffer = Buffer.from(arrayBuffer);
