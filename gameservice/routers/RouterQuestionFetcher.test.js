32a0e52d2cc4c1b7e088359aa77eb528e8095cc4
DavidPedregal <uo293738@uniovi.es>
2025-03-08 21:08:11 +0100

    #67 - Test case 1

--- gameservice/routers/RouterQuestionFetcher.test.js
+++ gameservice/routers/RouterQuestionFetcher.test.js
@@ -1,5 +1,4 @@
 const request = require('supertest');
-const fetcher = require('./RouterQuestionFetcher');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
 let app;
@@ -15,3 +14,24 @@ afterAll(async () => {
     app.close();
     await mongoServer.stop();
 });
+
+describe('Question Fetcher', () => {
+    it('should fetch data from Wikidata', async () => {
+        const response = await request(app).get('/generate/Q515/5');
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('message', '✅ Data fetched successfully');
+    });
+
+    it('should return an error when failing to fetch data (non existing code)', async () => {
+        const response = await request(app).get('/generate/Q000/5');
+        expect(response.status).toBe(500);
+        expect(response.body).toHaveProperty('error', '❌ Failed to retrieve data');
+    });
+
+    it('should return 5 questions)', async () => {
+        const response = await request(app).get('/questions/Q515');
+        expect(response.status).toBe(200);
+        expect(response.body.items).toHaveLength(5);
+    });
+}
+);
