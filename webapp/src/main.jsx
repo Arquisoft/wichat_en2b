11dd5a93f3c34969ca9e34fe7ee3b30dbb3af137
adrianmfuentes <amf13azul@proton.me>
2025-03-17 16:36:52 +0100

    #95 - Initialize Vite and React setup with Tailwind CSS integration

---
+++ webapp/src/main.jsx
@@ -0,0 +1,10 @@
+import './styles/index.css'
+import App from './App.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
