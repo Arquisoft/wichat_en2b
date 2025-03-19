8b10da30fda92ce5a5c494bef061a39d74b7b11e
Carlos Fernández Martínez <UO293564@uniovi.es>
2025-03-19 11:26:39 +0100

    #76 - Add robot button to QuestionGame with styling updates

--- webapp/src/components/game/QuestionGame.js
+++ webapp/src/components/game/QuestionGame.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import "../../styles/QuestionGame.css";
-import { Alert, CircularProgress } from "@mui/material";
+import { Alert, CircularProgress, Fab } from "@mui/material";
+import { FaRobot } from "react-icons/fa";
 const gatewayService = process.env.GATEWAY_SERVICE_URL || 'http://localhost:8004';
 
 export default function QuestionGame(params) {
@@ -83,6 +84,11 @@ export default function QuestionGame(params) {
                             </button>
                         ))}
                     </div>
+
+                    {/* Add the robot button here */}
+                    <Fab size="large" color="secondary" aria-label="add" className="robot-button">
+                        <FaRobot size={30} />
+                    </Fab>
                 </div>
             )}
             <div className="divider"></div>
