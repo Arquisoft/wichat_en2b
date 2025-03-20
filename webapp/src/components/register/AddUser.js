0ac83d38ba9b8be6cad32dd840fa8a38d47d04ce
DavidPedregal <uo293738@uniovi.es>
2025-03-20 22:01:27 +0100

    Password fields error shown

--- webapp/src/components/register/AddUser.js
+++ webapp/src/components/register/AddUser.js
@@ -8,6 +8,7 @@ const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000
 const AddUser = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
   const [role, setRole] = useState('USER');
   const [date, setDate] = useState(Date.now());
   const [error, setError] = useState('');
@@ -16,7 +17,7 @@ const AddUser = () => {
   const addUser = async () => {
     try {
       setDate(Date.now());
-      await axios.post(`${apiEndpoint}/adduser`, { username, password, role, date });
+      await axios.post(`${apiEndpoint}/users`, { username:username, password:password, role:role, createdAt:date });
       setOpenSnackbar(true);
     } catch (error) {
       setError(error.response.data.error);
@@ -34,21 +35,69 @@ const AddUser = () => {
       </Typography>
       <TextField
         name="username"
-        margin="normal"
         fullWidth
+        required
+        id="standard-required"
         label="Username"
+        variant="standard"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
-      <TextField
-        name="password"
-        margin="normal"
-        fullWidth
-        label="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      if (password !== passwordConfirm) {
+        <TextField
+          name="password"
+          fullWidth
+          required
+          id="password-input"
+          label="Password"
+          type="password"
+          variant="standard"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      } else {
+        <TextField
+          name="password"
+          fullWidth
+          required
+          id="password-input"
+          label="Password"
+          type="password"
+          variant="filled"
+          color="success"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      }
+      if (password !== passwordConfirm) {
+        <TextField
+          name="passwordConfirm"
+          error
+          fullWidth
+          required
+          id="passwordConfirm-input"
+          label="Confirm password"
+          type="password"
+          variant="standard"
+          helperText="Passwords do not match"
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+        />
+      } else {
+        <TextField
+          name="passwordConfirm"
+          fullWidth
+          required
+          id="passwordConfirm-input"
+          label="Confirm password"
+          type="password"
+          variant="filled"
+          color="success"
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+        />
+      }
+             
       <Button variant="contained" color="primary" onClick={addUser}>
         Add User
       </Button>
