ce05db1c8e1ed75928c4baf383c92d28aa88156a
DavidPedregal <uo293738@uniovi.es>
2025-03-22 11:25:58 +0100

    #92 Coppied configuration from #93 to fix webapp errors

--- webapp/jest.setup.js
+++ webapp/jest.setup.js
@@ -1,12 +1,17 @@
-import '@testing-library/jest-dom';
-import fetchMock from 'jest-fetch-mock';
+const fetchMock = require('jest-fetch-mock');
 
+// Extiende jest-dom
+require('@testing-library/jest-dom');
+
+// Habilita mocks para fetch
 fetchMock.enableMocks();
 
+// Resetea mocks antes de cada test
 beforeEach(() => {
   fetchMock.resetMocks();
 });
 
+// Mock para scrollIntoView en entornos donde `window` está definido
 if (typeof window !== 'undefined') {
   Element.prototype.scrollIntoView = jest.fn();
 }
