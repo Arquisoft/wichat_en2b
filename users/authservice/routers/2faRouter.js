18913eac9a35b5744cf6a52cd6deaf570bab82be
HovanRojasIgnacio <nachohovan@gmail.com>
2025-03-24 18:46:20 +0100

    #106 - Basic 2fa setup

--- users/authservice/routers/2faRouter.js
+++ users/authservice/routers/2faRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const otplib = require('otplib');
+const qrcode = require("qrcode")
 
 require('dotenv').config(); 
 
@@ -10,11 +11,22 @@ const router = express.Router();
 router.post('/setup2fa',
   async (req, res) => {
     try {
-       
+      const secret = otplib.authenticator.generateSecret();
+      qrcode.toDataURL(
+        otplib.authenticator.keyuri(req.user,"wichat_en2b",secret),
+        (err, imageUrl) => {
+          if(err){
+            return res.status(500).send("Error generating QR code");
+          }
+          res.send({ secret, imageUrl})
+        }
+      )
 
     } catch (error) {
      
     }
 });
+
+
 
 module.exports = router;
