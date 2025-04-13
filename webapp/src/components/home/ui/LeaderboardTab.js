a29d5fc8a22185562c6d0272cde8ece2d782fec7
DavidPedregal <uo293738@uniovi.es>
2025-04-13 10:40:14 +0200

    #160 - Fixed duplicated code

--- webapp/src/components/home/ui/LeaderboardTab.js
+++ webapp/src/components/home/ui/LeaderboardTab.js
@@ -29,99 +29,100 @@ const apiEndpoint = process.env.NEXT_PUBLIC_GATEWAY_SERVICE_URL || 'http://local
  */
 export default function LeaderboardTab() {
     const [leaderboard, setLeaderboard] = useState(null);
-    const [groupLeaderboard, setGroupLeaderboard] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [player, setPlayer] = useState(null);
     const [tabIndex, setTabIndex] = useState(0);
     const [doesGroupExist, setDoesGroupExist] = useState(false);
 
-    useEffect(() => {
-        const fetchLeaderboard = async () => {
-            setLoading(true);
+    const fetchLeaderboard = async () => {
+        setLoading(true);
 
-            try {
-                const data = await fetchWithAuth("/leaderboard");
-                if (!data || !data.leaderboard) {
-                    setError("No leaderboard data available.");
-                }
-                const token = getAuthToken();
-                const currentPlayerId = await getCurrentPlayerId(token);
-
-                setPlayer(currentPlayerId);
-                console.log("Leaderboard data:", data.leaderboard);
-                setLeaderboard(data.leaderboard);
-            } catch (error) {
-                setError(error.message || "Failed to fetch leaderboard data.");
-            } finally {
-                setLoading(false);
+        try {
+            const data = await fetchWithAuth("/leaderboard");
+            if (!data || !data.leaderboard) {
+                setError("No leaderboard data available.");
             }
-        }
-        fetchLeaderboard();
-
-        const fetchGroupLeaderboard = async () => {
-            setLoading(true);
-
             const token = getAuthToken();
-            try {
-                const groupFetch = await axios.get(
-                    `${apiEndpoint}/groups/joined`,
-                    {
-                      headers: {
-                        Authorization: `Bearer ${token}`,
-                        "Content-Type": "application/json",
-                      },
-                    }
-                );
+            const currentPlayerId = await getCurrentPlayerId(token);
 
-                const group = groupFetch.data;
-                if (!group) {
-                    setGroupLeaderboard(null);
-                    setDoesGroupExist(false);
-                    return setError("No group data available.");
-                }
-                setDoesGroupExist(true);
-
-                const playerIds = group.members;
-                console.log("Group members:", playerIds);
-
-                const players = await axios.post(
-                    `${apiEndpoint}/users/by-ids`,
-                    { users: playerIds },
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                            "Content-Type": "application/json",
-                        },
-                    }
-                );
-
-                const playerNames = players.data.map((player) => player.username);
-                console.log("Player names:", playerNames);
-
-                const response = await axios.post(
-                    `${apiEndpoint}/leaderboard/group`,
-                    { players: playerNames },
-                    {
-                        headers: {
-                        Authorization: `Bearer ${token}`,
-                        "Content-Type": "application/json",
-                        },
-                    }
-                );
-                console.log("Group leaderboard data:", response.data.leaderboard);
-                setGroupLeaderboard(response.data.leaderboard);
-            } catch (error) {
-                setGroupLeaderboard(null);
-                if (error.response && error.response.status !== 404) {
-                    setError(error.message || "Failed to fetch group leaderboard data.");
-                }
-            } finally {
-                setLoading(false);
-            }
+            setPlayer(currentPlayerId);
+            console.log("Leaderboard data:", data.leaderboard);
+            setLeaderboard(data.leaderboard);
+        } catch (error) {
+            setError(error.message || "Failed to fetch leaderboard data.");
+        } finally {
+            setLoading(false);
         }
-        fetchGroupLeaderboard();
+    }
+
+    const fetchGroupLeaderboard = async () => {
+        setLoading(true);
+
+        const token = getAuthToken();
+        try {
+            const groupFetch = await axios.get(
+                `${apiEndpoint}/groups/joined`,
+                {
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                  },
+                }
+            );
+
+            const group = groupFetch.data;
+            if (!group) {
+                setLeaderboard(null);
+                setDoesGroupExist(false);
+                return setError("No group data available.");
+            }
+            setDoesGroupExist(true);
+
+            const playerIds = group.members;
+            console.log("Group members:", playerIds);
+
+            const players = await axios.post(
+                `${apiEndpoint}/users/by-ids`,
+                { users: playerIds },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            const playerNames = players.data.map((player) => player.username);
+            console.log("Player names:", playerNames);
+
+            const response = await axios.post(
+                `${apiEndpoint}/leaderboard/group`,
+                { players: playerNames },
+                {
+                    headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                    },
+                }
+            );
+            console.log("Group leaderboard data:", response.data.leaderboard);
+            setLeaderboard(response.data.leaderboard);
+        } catch (error) {
+            setLeaderboard(null);
+            if (error.response && error.response.status !== 404) {
+                setError(error.message || "Failed to fetch group leaderboard data.");
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchLeaderboard();
     }, [])
+
+
     return (
         <div className="leaderboard-tab">
             {/* Tabs */}
@@ -133,136 +134,76 @@ export default function LeaderboardTab() {
                     variant="scrollable"
                     className={"tabs-header"}
                 >
-                    <Tab label="WiChat Leaderboard" icon={<PublicIcon/>} />
-                    <Tab label="Group Leaderboard" icon={<GroupIcon />} />
+                    <Tab label="WiChat Leaderboard" icon={<PublicIcon/>} onClick={fetchLeaderboard}/>
+                    <Tab label="Group Leaderboard" icon={<GroupIcon />} onClick={fetchGroupLeaderboard}/>
                 </Tabs>
             </div>
         
-            {tabIndex === 0 && (
-                <Card>
-                <CardHeader title="WiChat Leaderboard" />
-                <LoadingErrorHandler loading={loading} error={error}>
-                    <CardContent className="card-content">
-                        <CardContent className="card-content">
-                            <TableContainer component={Paper}>
-                                <Table aria-label="leaderboard table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Rank</TableCell>
-                                            <TableCell>Username</TableCell>
-                                            <TableCell align="right">Total Score</TableCell>
-                                            <TableCell align="right">Games Played</TableCell>
-                                            <TableCell align="right">Average Score</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {leaderboard &&
-                                            leaderboard.map((entry) => {
-                                                const isCurrentPlayer =
-                                                    player && (player === entry._id || (typeof player === "object" && player._id === entry._id))
-
-                                                return (
-                                                    <TableRow
-                                                        key={entry._id}
-                                                        className={isCurrentPlayer ? "current-player" : ""}
-                                                        sx={{
-                                                            backgroundColor: isCurrentPlayer ? "rgba(144, 202, 249, 0.2)" : "inherit",
-                                                            "&:hover": {
-                                                                backgroundColor: isCurrentPlayer ? "rgba(144, 202, 249, 0.3)" : "rgba(0, 0, 0, 0.04)",
-                                                            },
-                                                        }}
-                                                    >
-                                                        <TableCell>#{entry.rank}</TableCell>
-                                                        <TableCell>
-                                                            {isCurrentPlayer ? (
-                                                                <Typography component="span" fontWeight="bold">
-                                                                    {entry._id} (You)
-                                                                </Typography>
-                                                            ) : (
-                                                                entry._id
-                                                            )}
-                                                        </TableCell>
-                                                        <TableCell align="right">{`${entry.totalScore.toLocaleString()} points`}</TableCell>
-                                                        <TableCell align="right">{entry.totalGames}</TableCell>
-                                                        <TableCell align="right">{`${entry.avgScore.toFixed(1)} points`}</TableCell>
-                                                    </TableRow>
-                                                )
-                                            })}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                        </CardContent>
-                    </CardContent>
-                </LoadingErrorHandler>
-                </Card>
-            )}
-
             {tabIndex === 1 && (
                 <div className="group-leaderboard">
                     {!doesGroupExist && (
                         <CardHeader title="You do not belong to any group!" />
                     )}
-
-                    {doesGroupExist && (
-                        <Card>
-                        <CardHeader title="Group Leaderboard" />
-                        <LoadingErrorHandler loading={loading} error={error}>
-                        <CardContent className="card-content">
-                            <CardContent className="card-content">
-                                <TableContainer component={Paper}>
-                                    <Table aria-label="group leaderboard table">
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell>Rank</TableCell>
-                                                <TableCell>Username</TableCell>
-                                                <TableCell align="right">Total Score</TableCell>
-                                                <TableCell align="right">Games Played</TableCell>
-                                                <TableCell align="right">Average Score</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {groupLeaderboard &&
-                                                groupLeaderboard.map((entry) => {
-                                                    const isCurrentPlayer =
-                                                        player && (player === entry._id || (typeof player === "object" && player._id === entry._id))
-    
-                                                    return (
-                                                        <TableRow
-                                                            key={entry._id}
-                                                            className={isCurrentPlayer ? "current-player" : ""}
-                                                            sx={{
-                                                                backgroundColor: isCurrentPlayer ? "rgba(144, 202, 249, 0.2)" : "inherit",
-                                                                "&:hover": {
-                                                                    backgroundColor: isCurrentPlayer ? "rgba(144, 202, 249, 0.3)" : "rgba(0, 0, 0, 0.04)",
-                                                                },
-                                                            }}
-                                                        >
-                                                            <TableCell>#{entry.rank}</TableCell>
-                                                            <TableCell>
-                                                                {isCurrentPlayer ? (
-                                                                    <Typography component="span" fontWeight="bold">
-                                                                        {entry._id} (You)
-                                                                    </Typography>
-                                                                ) : (
-                                                                    entry._id
-                                                                )}
-                                                            </TableCell>
-                                                            <TableCell align="right">{`${entry.totalScore.toLocaleString()} points`}</TableCell>
-                                                            <TableCell align="right">{entry.totalGames}</TableCell>
-                                                            <TableCell align="right">{`${entry.avgScore.toFixed(1)} points`}</TableCell>
-                                                        </TableRow>
-                                                    )
-                                                })}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
-                            </CardContent>
-                        </CardContent>
-                    </LoadingErrorHandler>
-                    </Card>
-                    )}
                 </div>
             )}
+            
+            <Card>
+            <CardHeader title="WiChat Leaderboard" />
+            <LoadingErrorHandler loading={loading} error={error}>
+                <CardContent className="card-content">
+                    <CardContent className="card-content">
+                        <TableContainer component={Paper}>
+                            <Table aria-label="leaderboard table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Rank</TableCell>
+                                        <TableCell>Username</TableCell>
+                                        <TableCell align="right">Total Score</TableCell>
+                                        <TableCell align="right">Games Played</TableCell>
+                                        <TableCell align="right">Average Score</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {leaderboard &&
+                                        leaderboard.map((entry) => {
+                                            const isCurrentPlayer =
+                                                player && (player === entry._id || (typeof player === "object" && player._id === entry._id))
+
+                                            return (
+                                                <TableRow
+                                                    key={entry._id}
+                                                    className={isCurrentPlayer ? "current-player" : ""}
+                                                    sx={{
+                                                        backgroundColor: isCurrentPlayer ? "rgba(144, 202, 249, 0.2)" : "inherit",
+                                                        "&:hover": {
+                                                            backgroundColor: isCurrentPlayer ? "rgba(144, 202, 249, 0.3)" : "rgba(0, 0, 0, 0.04)",
+                                                        },
+                                                    }}
+                                                >
+                                                    <TableCell>#{entry.rank}</TableCell>
+                                                    <TableCell>
+                                                        {isCurrentPlayer ? (
+                                                            <Typography component="span" fontWeight="bold">
+                                                                {entry._id} (You)
+                                                            </Typography>
+                                                        ) : (
+                                                            entry._id
+                                                        )}
+                                                    </TableCell>
+                                                    <TableCell align="right">{`${entry.totalScore.toLocaleString()} points`}</TableCell>
+                                                    <TableCell align="right">{entry.totalGames}</TableCell>
+                                                    <TableCell align="right">{`${entry.avgScore.toFixed(1)} points`}</TableCell>
+                                                </TableRow>
+                                            )
+                                        })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </CardContent>
+                </CardContent>
+            </LoadingErrorHandler>
+            </Card>
+            
         </div>
     )
 }
