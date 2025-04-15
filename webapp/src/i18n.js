41e438e3bff5f51109d94264c05d32c93fdf0d1c
DavidPedregal <uo293738@uniovi.es>
2025-04-15 13:05:56 +0200

    #155 - Internazionalization is working

--- webapp/src/i18n.js
+++ webapp/src/i18n.js
@@ -3,10 +3,22 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
+import translationEn from "./locales/en/translation.json";
+import translationEs from "./locales/es/translation.json";
+
 i18next
     .use(initReactI18next)
     .use(LanguageDetector)
     .use(Backend)
     .init({
         fallbackLng: "en",
+        resources: {
+            en: { translation: translationEn },
+            es: { translation: translationEs },
+        },
+        interpolation: {
+        escapeValue: false,
+        },
     })
+
+export default i18next;
